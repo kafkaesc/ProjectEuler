9a3a35727095abb3400c3a4ba1639941d40ca89f
kafkaesc <openquote@outlook.com>
2015-09-28 17:12:12 -0500

    solution for problem 13

---
+++ src/E13.java
@@ -0,0 +1,39 @@
+/**
+ * Created by jared on 9/28/15.
+ *
+ * Project Euler, problem #13
+ *
+ */
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.math.BigInteger;
+
+public class E13 {
+
+    private static String[] input;
+    private static File E13input = new File("src/E13input.txt");;
+
+    public static void main(String[] args) {
+        try {
+            loadInput();
+
+            BigInteger answer = BigInteger.ZERO;
+            BigInteger bi = BigInteger.ZERO;
+            for(int i = 0; i < input.length; i++) {
+                bi = new BigInteger(input[i]);
+                answer = answer.add(bi);
+            }
+            BigInteger divisor = new BigInteger("1000000000000000000000000000000000000000000");
+            answer = answer.divide(divisor);
+            System.out.println(answer);
+
+        } catch (FileNotFoundException fne) {
+            System.out.println("Check the location of file E13input.txt");
+        }
+    }
+
+    private static void loadInput() throws FileNotFoundException {
+        input = ArrayMaker.string1D(E13input);
+    }
+}
